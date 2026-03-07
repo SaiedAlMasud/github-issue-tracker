8c4629880ca99b7db6498ad5b278f820bd2ae1f3
Ankon 190 <ankon190@gmail.com>
2026-03-07 19:57:30 +0600

    challange part 1 border color added successfully

--- script/home.js
+++ script/home.js
@@ -61,8 +61,15 @@ const displayIssues = (issues, totalIssues) => {
         
         const issuecard = document.createElement("div");
         issuecard.classList.add("rounded-md", "shadow-md", "bg-white", "p-5");
+        if(issue.status === "open"){
+            issuecard.classList.add("border-green-500", "border-t-4");
+        }
+        else if(issue.status === "closed"){
+            issuecard.classList.add("border-red-500", "border-t-4");
+        }
+        issuecard.onclick = () => handleIssueClick(issue.id);
         issuecard.innerHTML = `
-                        <div class="flex justify-between p-5">
+                        <div class="flex justify-between items-center">
                             <img class="w-8 h-8" src="${issue.status === 'open' ? '../assets/Open-Status.png' : '../assets/Closed-Status.png'}" alt="">
                             <div class="px-4 flex justify-center items-center bg-${issue.priority === 'high' ? 'red-300' : issue.priority === 'medium' ? '[#FFF8DB]' : '[#EEEFF2]'} 
                             text-${issue.priority === 'high' ? 'red-600' : issue.priority === 'medium' ? '[#D97706]' : '[#5B5F67]'} rounded-2xl">
@@ -91,5 +98,40 @@ const displayIssues = (issues, totalIssues) => {
     });
 }
 
+const handleIssueClick = async (id) => {
+    const url = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`;
+    // Implement the logic to fetch and display issue details
+    const res = await fetch(url);
+    const data = await res.json();
+    showIssueDetails(data.data);
+};
+
+const showIssueDetails = (issue) => {
+    const issueDetailsContainer = document.getElementById("issue-details-container");
+    issueDetailsContainer.innerHTML = `
+        <h2 class="text-2xl font-bold">${issue.title}</h2>
+                        <div class="flex gap-2">
+                            <div class="px-2 text-white bg-green-500 rounded-lg text-[13px]">${issue.status}ed</div>
+                            <p class="text-gray-500 text-[13px]">Opened by ${issue.author}</p>
+                            <p class="text-gray-500 text-[13px]">Created At: ${issue.createdAt}</p>
+                        </div>
+                        <div class="flex gap-3 my-4">
+                            ${issue.labels.map(label => `<div class="bg-[#FFF8DB] text-[#D97706] rounded-xl px-2">${label}</div>`).join('')}
+                        </div>
+                        <p class="text-gray-500 mb-5">${issue.description}</p>
+                        <div class="flex justify-between mx-5 bg-base-200 rounded-lg p-4">
+                            <div class="mx-5">
+                                <p class="text-gray-500">Assignee: </p>
+                                <h3 class="font-bold">${issue.author}</h3>
+                            </div>
+                            <div class="mx-5">
+                                <p class="text-gray-500">Priority: </p>
+                                <h3 class="flex justify-center bg-red-500 text-white rounded-xl px-2">${issue.priority}</h3>
+                            </div>
+                        </div>
+    `;
+    document.getElementById("my_modal_5").showModal();
+
+}
 
 loadAllIssues();
